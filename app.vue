a9e9e0068dfe19f79835f41d9f3537fb2e1bf60d
cool-pants <muban.sg@gmail.com>
2022-06-09 13:21:15 +0530

    [FEAT]: Add homepage scaffold

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
+<script>
+export default {}
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <h1>Home Page</h1>
+  <h2>Recently Viewed</h2>
+  <h2>Workspaces</h2>
 </template>
+
+<style>
+</style>
